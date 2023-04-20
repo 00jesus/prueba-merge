3e2fe3715ee90511678de894976befeeb81bdba9
00jesus <j.acevedo03@ufromail.cl>
2023-04-20 12:48:49 -0400

    segundo commit, metodos

---
+++ src/main/java/Aritmetica.java
@@ -0,0 +1,12 @@
+public class Aritmetica {
+    public static void main(String[] args) {
+
+    }
+
+    public static int sumar (int a, int b)  {
+        return a+b;
+    }
+    public static int multiplicar (int a, int b) {
+        return a*b;
+    }
+}
