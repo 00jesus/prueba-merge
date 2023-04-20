b6deb714329f45d30b0917e23b0af3f3462b1c83
00jesus <j.acevedo03@ufromail.cl>
2023-04-20 12:52:00 -0400

    tercer commit, rama develop

---
+++ src/main/java/Literales.java
@@ -0,0 +1,12 @@
+public class Literales {
+    public static void main(String[] args) {
+
+    }
+    public static void imprimir () {
+        System.out.println("palabras impresas");
+    }
+    public static double division (double a, double b) {
+        return a/b;
+    }
+}
+
